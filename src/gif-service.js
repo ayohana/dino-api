8afd615d16601d7a35412c40c73eb99f88c0c2e0
Adela Darmansyah and Krista Rutz <adela@>
2020-02-12 11:29:41 -0800

    add a feature to display dino gifs

---
+++ src/gif-service.js
@@ -0,0 +1,14 @@
+export class Giphy{
+  async getDinoGif(){
+    try {
+      console.log("test");
+      let response = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=Apy92VYBwVhDyFQCTYl8oyootNUw4Efd&tag=dinosaur&rating=pg`);
+      console.log("help")
+      let jsonifiedResponse = await response.json();
+      console.log("response -", jsonifiedResponse);
+      return jsonifiedResponse
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,23 +3,27 @@ import 'bootstrap' ;
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import './styles.css' ;
 import { Dino } from '../src/dino-service';
+import { Giphy } from './gif-service';
 
 $(document).ready(function() {
   $("button#newGame").click(function() {
 
     (async () => {
       let dino = new Dino();
-      const response = await dino.getRandomDino();
-      displayWord(response);
+      let gif = new Giphy();
+      const response1 = await dino.getRandomDino();
+      const response2 = await gif.getDinoGif();
+      displayOnScreen(response1, response2);
     })();
 
-    const displayWord = function(response) {
-      $("#output").text(response[0]);
+    const displayOnScreen = function(response1, response2){
+      $("#output").text(response1[0]);
+      let image = new Image();
+      image.id = "dinoGif";
+      image.src = response2.data.images.downsized_large.url;
+      $("#gifOutput").html(image);
+      //console.log(response2);
+      //$("#gifOutput").text
     }
-
-
   });
-
-
-
 });
